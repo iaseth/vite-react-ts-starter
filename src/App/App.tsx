6c964e1de7f8eb4807a6fd3a344e16144d61f01c
Ankur Seth <iankurseth@gmail.com>
2023-05-14 10:30:33 +0530

    Added tailwind
    Updated project structure.
    Removed default CSS and Markup.

---
+++ src/App/App.tsx
@@ -0,0 +1,17 @@
+import './App.css';
+
+
+
+function App () {
+
+	return (
+		<div className="min-h-screen bg-green-100 text-white flex">
+			<a href="https://github.com/iaseth/vite-react-ts-starter" className="m-auto bg-green-600 text-center px-8 py-6 rounded">
+				<h1 className="H3">Start</h1>
+				<p className="read-the-docs">vite-react-ts-starter</p>
+			</a>
+		</div>
+	);
+}
+
+export default App;
