bd9eeae8cafe786e51b1ed214744844dc1fb1eea
Ankur Seth <iankurseth@gmail.com>
2024-04-04 14:53:44 +0530

    Restructuring #1 - Moved Header and Footer into components folder

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,20 +1,18 @@
 import './App.css';
 
-import Footer from './Footer/Footer';
-import Header from './Header/Header';
-import Main from './Main/Main';
+import MainPage from './pages/MainPage/MainPage';
 
 import { red } from 'redicons';
 import rediconsJson from './redicons.json';
+
+
 red.addIcons(rediconsJson.icons);
 
 export default function App () {
 
 	return (
 		<div className="bg-zinc-900 text-white">
-			<Header />
-			<Main />
-			<Footer />
+			<MainPage />
 		</div>
 	);
 }
